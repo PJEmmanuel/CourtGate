176c27152bcc06ae88c3b1b03529b5dc371d51d7
PJEmmanuel <emma_piloto@hotmail.com>
2025-06-07 16:32:21 +0200

    add archives

---
+++ app/src/main/java/com/example/courtgate/authentication/presentation/login/components/LoginForm.kt
@@ -0,0 +1,102 @@
+package com.example.courtgate.authentication.presentation.login.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import com.example.courtgate.authentication.presentation.login.LoginEvent
+import com.example.courtgate.authentication.presentation.login.LoginState
+import com.example.courtgate.core.presentation.CourtTextField
+import com.example.courtgate.core.presentation.PasswordTextField
+import com.example.courtgate.core.presentation.CourtButton
+
+@Composable
+fun LoginForm(
+    state: LoginState,
+    onEvent: (LoginEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Log in with email",
+            style = MaterialTheme.typography.labelLarge
+        )
+        CourtTextField(
+            value = state.email,
+            onValueChange = { onEvent(LoginEvent.EmailChange(it)) },
+            placeholder = "Email",
+            contentDescription = "Enter email",
+            leadingIcon = Icons.Default.Email,
+            keyboardOptions = KeyboardOptions(
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(),//TODO : terminar!!!!!!!!!!!!!!!!
+            errorMessage = state.emailError,
+            isEnabled = !state.isLoading,
+            modifier = Modifier
+        )
+        Spacer(Modifier.padding(2.dp))
+        PasswordTextField(
+            value = state.password,
+            onValueChange = { onEvent(LoginEvent.PasswordChange(it)) },
+            contentDescription = "Enter password",
+            keyboardOptions = KeyboardOptions(
+                autoCorrectEnabled = false,
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(),//TODO : terminar!!!!!!!!!!!!!!!!
+            errorMessage = state.passwordError,
+            isEnabled = !state.isLoading,
+            modifier = Modifier
+        )
+
+        CourtButton(
+            text = "Login",
+            modifier = Modifier.fillMaxWidth(),
+            isEnabled = !state.isLoading
+        ) {
+            onEvent(LoginEvent.Login)
+        }
+
+        Spacer(Modifier.padding(4.dp))
+
+        TextButton(
+            onClick = {}
+        ) {
+            Text("Forgot Password?", textDecoration = TextDecoration.Underline)
+        }
+
+        TextButton(
+            onClick = { onEvent(LoginEvent.SignUp) }
+        ) {
+            Text(buildAnnotatedString {
+                append("Don’t have an account? ")
+                withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                    append("Sign up")
+                }
+            })
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/courtgate/core/presentation/CourtTextField.kt
@@ -0,0 +1,107 @@
+package com.example.courtgate.core.presentation
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PasswordTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String = "Password",
+    contentDescription: String,
+    modifier: Modifier,
+    errorMessage: String? = null,
+    leadingIcon: ImageVector? = Icons.Default.Lock,
+    isEnabled: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(),
+    keyboardActions: KeyboardActions = KeyboardActions(),
+) {
+    CourtTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = placeholder,
+        contentDescription = contentDescription,
+        modifier = modifier,
+        errorMessage = errorMessage,
+        leadingIcon = leadingIcon,
+        isPassword = true,
+        isEnabled = isEnabled,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+    )
+}
+
+//TODO: Cómo mostrar o dónde se muestra el texto de error
+@Composable
+fun CourtTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    contentDescription: String,
+    modifier: Modifier,
+    errorMessage: String? = null,
+    leadingIcon: ImageVector? = null,
+    isPassword: Boolean = false,
+    isEnabled: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(),
+    keyboardActions: KeyboardActions = KeyboardActions(),
+) {
+    var hidePassword by remember { mutableStateOf(true) }
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text(placeholder) },
+        modifier = modifier
+            .fillMaxWidth()
+            .semantics { this.contentDescription = contentDescription },
+        leadingIcon = leadingIcon?.let { icon ->
+            {
+                Icon(
+                    imageVector = leadingIcon,
+                    contentDescription = ""
+                )
+            }
+        },
+        trailingIcon = {
+            if (isPassword) {
+                val textSelect = if (hidePassword) "show" else "hide"
+                TextButton(
+                    onClick = { hidePassword = !hidePassword },
+                    enabled = isEnabled
+                ) {
+                    Text(textSelect, textDecoration = TextDecoration.Underline)
+                }
+            }
+        },
+        isError = errorMessage != null,
+        enabled = isEnabled,
+        singleLine = true,
+        shape = RoundedCornerShape(12.dp),
+        visualTransformation = if (isPassword && hidePassword) PasswordVisualTransformation() else VisualTransformation.None,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
+    )
+
+}

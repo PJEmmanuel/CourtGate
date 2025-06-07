a44bb56ca1e72855eb54c7d516d7fa3b9cf15664
PJEmmanuel <emma_piloto@hotmail.com>
2025-06-07 16:32:21 +0200

    add archives

---
+++ app/src/main/java/com/example/courtgate/authentication/presentation/register/components/RegisterForm.kt
@@ -0,0 +1,8 @@
+package com.example.courtgate.authentication.presentation.register.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterForm() {
+
+}
---
+++ app/src/main/java/com/example/courtgate/core/presentation/CourtButton.kt
@@ -0,0 +1,21 @@
+package com.example.courtgate.core.presentation
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CourtButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    Button(onClick = onClick, modifier = modifier, shape = RoundedCornerShape(8.dp)) {
+        Text(text = text, style = MaterialTheme.typography.labelMedium)
+    }
+}

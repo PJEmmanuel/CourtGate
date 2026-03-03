24e218a9e127fe127477b64a1125e7ec351f22b6
PJEmmanuel <emma_piloto@hotmail.com>
2026-03-03 13:20:31 +0100

    new homeViewModel

--- app/src/main/java/com/example/courtgate/ui/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/courtgate/ui/presentation/home/HomeViewModel.kt
@@ -1,32 +1,29 @@
 package com.example.courtgate.ui.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.courtgate.ResultCourt
+import com.example.courtgate.domain.models.LastResult
 import com.example.courtgate.usecases.home.GetLastResultUseCase
-import com.example.courtgate.usecases.home.HomeUsesCases
-import com.example.courtgate.usecases.home.InsertLastResultUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     getLastResultUseCase: GetLastResultUseCase,
-    private val insertLastResultUseCase: InsertLastResultUseCase
 ) : ViewModel() {
 
-    private val _state = MutableStateFlow<HomeState>(HomeState())
-    val state: StateFlow<HomeState> = _state.asStateFlow()
-
+    val state: StateFlow<ResultCourt<List<LastResult>>> = getLastResultUseCase.invoke()
+        .map<List<LastResult?>, ResultCourt<List<LastResult>>> { ResultCourt.Success(it.filterNotNull()) }
+        .catch { emit(ResultCourt.Error(it)) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ResultCourt.Loading
+        )
 }
-
-/*
-data class SetResult(
-    val firstResult: Int,
-    val secondResult: Int,
-)
-
-data class LastMatchResult(
-    val match: List<SetResult>
-)*/

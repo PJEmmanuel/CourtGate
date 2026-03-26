7e6fecd4c1625b3501dacf7e079ab2189ae4f4a8
PJEmmanuel <emma_piloto@hotmail.com>
2026-03-26 15:48:24 +0100

    Refactor ManageCourtRepository

--- app/src/main/java/com/example/courtgate/data/ManageCourtRepository.kt
+++ app/src/main/java/com/example/courtgate/data/ManageCourtRepository.kt
@@ -1,13 +1,15 @@
 package com.example.courtgate.data
 
+import com.example.courtgate.ResultManage
 import com.example.courtgate.data.datasources.CourtLocalDataSource
 import com.example.courtgate.data.datasources.CourtRemoteDataSource
-import com.example.courtgate.di.ApplicationScope
 import com.example.courtgate.domain.models.Court
+import com.example.courtgate.domain.models.DomainError
+import com.example.courtgate.domain.models.DomainException
 import com.example.courtgate.domain.models.FilterOption
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
@@ -17,92 +19,45 @@ import javax.inject.Inject
 class ManageCourtRepository @Inject constructor(
     private val remoteDataSource: CourtRemoteDataSource,
     private val localDataSource: CourtLocalDataSource,
-    @ApplicationScope private val externalScope: CoroutineScope
 ) {
-
-    /* fun getAllCourtToShow(
-         located: String?,
-         selectedDay: Long,
-         endSelectedDay: Long,
-         currentDayStart: Instant,
-         endSevenDaysFromNow: Instant
-     ): Flow<List<Court>> = flow {
-
-         // Disparamos la sincronización en segundo plano sin bloquear el flujo
-         // Esto asegura que la UI vea lo que hay en Room inmediatamente
-         coroutineScope {
-             launch {
-                 try {
-                     syncRemoteData(
-                         selectedDay = selectedDay,
-                         endSelectedDay = endSelectedDay,
-                         currentDayStart = currentDayStart,
-                         endSevenDaysFromNow = endSevenDaysFromNow
-                     )
-                 } catch (e: Exception) { //TODO:Hay que mandar el error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                     // Loguear error de red, pero no romper el Flow de la DB
-                 }
-             }
-         }
-         // Retornamos el stream de la base de datos (SSOT)
-         emitAll(localDataSource.getAvailableCourts(located, selectedDay, endSelectedDay))
-     }.flowOn(Dispatchers.IO)//TODO NEcesario flowOn?*/
     fun getAllCourtToShow(
         located: String?,
         selectedDay: Long,
         endSelectedDay: Long,
         currentDayStart: Instant,
         endSevenDaysFromNow: Instant
-    ): Flow<List<Court>> {
+    ): Flow<List<Court>> = channelFlow {
 
-        // Lanzamos la sincronización en el scope externo
-        externalScope.launch {
-            try {
-                syncRemoteData(
-                    selectedDay,
-                    endSelectedDay,
-                    currentDayStart,
-                    endSevenDaysFromNow
-                )
-            } catch (e: Exception) {
-                // Loguear error, la UI no se rompe porque ya está escuchando Room
-            }
+        ensureStaticDataLoaded() //TODO: Actualiza en cada llamada
+
+        launch {
+            remoteDataSource.getBookingsSevenDaysAhead(currentDayStart, endSevenDaysFromNow)
+                .collect { bookings ->
+                    localDataSource.syncBookingsForDay(selectedDay, endSelectedDay, bookings)
+                }
         }
 
-        /* Retornamos directamente el flujo de Room.
-         Cuando syncRemoteData actualice Room, este Flow emitirá automáticamente.*/
-        return localDataSource.getAvailableCourts(located, selectedDay, endSelectedDay)
-            .distinctUntilChanged() // evitará emisiones si los datos son idénticos.
-            .flowOn(Dispatchers.IO)
-    }
+        localDataSource.getAvailableCourts(located, selectedDay, endSelectedDay)
+            .distinctUntilChanged()
+            .collect { courts -> send(courts) }
+    }.flowOn(Dispatchers.IO)
 
-    suspend fun getFilterOption(): List<FilterOption> = localDataSource.getDistinctLocatedTypes()
+    suspend fun getFilterOption(): ResultManage<List<FilterOption>, DomainError> {
+        return try {
+            ResultManage.Success(localDataSource.getDistinctLocatedTypes())
+        } catch (e: Exception) {
+            ResultManage.Failure(DomainError.Local.UnknownLocalError)
+        }
+    }
 
     private suspend fun ensureStaticDataLoaded() {
-        val remoteCourt = remoteDataSource.getAllCourt()
-        localDataSource.saveAllCourt(remoteCourt)
-
-        val schedule = remoteDataSource.getRegularHours()
-        localDataSource.saveRegularHours(schedule)
-    }
-
-    private suspend fun syncRemoteData(
-        selectedDay: Long,
-        endSelectedDay: Long,
-        currentDayStart: Instant,
-        endSevenDaysFromNow: Instant
-    ) {
-        ensureStaticDataLoaded()
-        // Sincronizamos reservas de Firestore a Room
-        remoteDataSource.getBookingsSevenDaysAhead(
-            currentDayStart = currentDayStart,
-            endSevenDaysFromNow = endSevenDaysFromNow
-        ).collect { bookings ->
-            localDataSource.syncBookingsForDay(
-                selectedDay = selectedDay,
-                endSelectedDay = endSelectedDay,
-                bookings = bookings
-            )
+        when (val courts = remoteDataSource.getAllCourt()) {
+            is ResultManage.Success -> localDataSource.saveAllCourt(courts.data)
+            is ResultManage.Failure -> throw DomainException(courts.error)
+        }
+        when (val schedule = remoteDataSource.getRegularHours()) {
+            is ResultManage.Success -> localDataSource.saveRegularHours(schedule.data)
+            is ResultManage.Failure -> throw DomainException(schedule.error)
         }
     }
 }

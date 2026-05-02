9bcf6d98220b92bc72029a2956375e0e21136fd4
PJEmmanuel <emma_piloto@hotmail.com>
2026-05-02 12:52:41 +0200

    refactor BookingVM

--- app/src/main/java/com/example/courtgate/ui/presentation/booking/BookingViewModel.kt
+++ app/src/main/java/com/example/courtgate/ui/presentation/booking/BookingViewModel.kt
@@ -1,110 +1,58 @@
-/*package com.example.courtgate.ui.presentation.booking
+package com.example.courtgate.ui.presentation.booking
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.courtgate.domain.models.FreeHoursOfCourt
+import com.example.courtgate.ResultCourt
+import com.example.courtgate.ResultManage
+import com.example.courtgate.di.CourtCode
+import com.example.courtgate.di.SelectedDay
+import com.example.courtgate.domain.models.DomainError
+import com.example.courtgate.domain.models.DomainException
+import com.example.courtgate.domain.models.NewCourtBooking
+import com.example.courtgate.usecases.booking.GetCourtSelectedByCodeUseCase
+import com.example.courtgate.usecases.booking.GetFreeHoursOnReservedCourtsUseCase
+import com.example.courtgate.usecases.booking.SetNewBookingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BookingViewModel @Inject constructor(
-    private val bookingUseCases: BookingUseCases
+    getCourtSelectedByCodeUseCase: GetCourtSelectedByCodeUseCase,
+    getFreeHoursOnReservedCourtsUseCase: GetFreeHoursOnReservedCourtsUseCase,
+    @CourtCode code: String,
+    @SelectedDay selectedDay: Long
 ) : ViewModel() {
 
-    private val _state = MutableStateFlow<BookingUiState<BookingState>>(BookingUiState.Idle)
-    val state: StateFlow<BookingUiState<BookingState>> = _state.asStateFlow()
-
-    fun fetchBookingData(code: String, date: String) {
-        viewModelScope.launch {
-            _state.value = BookingUiState.Loading
-
-            val courtResult = bookingUseCases.getCourtSelectedByCode.invoke(code)
-            val hoursResult = bookingUseCases.getFreeHoursOnReservedCourts.invoke(code, date)
-            _state.value = courtResult.fold(
-                onSuccess = { court ->
-                    hoursResult.fold(
-                        onSuccess = { hours ->
-                            Log.i("bookingVM", hours.toString())
-                            BookingUiState.Success(
-                                data = BookingState(
-                                    code = code,
-                                    requestedCourt = court,
-                                    freeHoursOfCourt = hours
-                                )
-                            )
-                        },
-                        onFailure = { BookingUiState.Error(it.message.orEmpty()) }
-                    )
-                },
-                onFailure = { BookingUiState.Error(it.message.orEmpty()) }
+    //TODO: Quitar ir a pantalla booking desde el botón reservas(es para gestionar reservas)
+    val state: StateFlow<ResultCourt<BookingState>> =
+        combine(
+            getCourtSelectedByCodeUseCase(code),
+            getFreeHoursOnReservedCourtsUseCase(code, selectedDay)
+        ) { court, freeHours ->
+            BookingState(
+                freeHoursOfCourt = freeHours,
+                requestedCourt = court,
             )
-        }
+        }.map<BookingState, ResultCourt<BookingState>> { ResultCourt.Success(it) }
+            .catch { e -> emit(ResultCourt.Error(mapError(e))) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = ResultCourt.Loading
+            )
+
+
+    //TODO sacar de aquí
+    private fun mapError(e: Throwable): DomainError = when (e) {
+        is DomainException -> e.error
+        else -> DomainError.Remote.UnknownRemoteError
     }
 
-    fun selectedHour(selectedHour: String?) {
-        _state.update { currentUiState ->
-            if (currentUiState is BookingUiState.Success) {
-
-                val oldData = currentUiState.data
-
-                val updateSelectedHour = onSelectedHourToReserve(
-                    listFreeHour = oldData.freeHoursOfCourt,
-                    selectedHour = selectedHour
-                )
-                currentUiState.copy(
-                    data = oldData.copy(
-                        freeHoursOfCourt = updateSelectedHour,
-                        selectedHourToBook = selectedHour ?: "00:00"
-                    )
-                )
-            } else currentUiState
-        }
-    }
-
-    fun activateAlertDialog() {
-        _state.update { currentUiState ->
-            if (currentUiState is BookingUiState.Success) {
-                val olData = currentUiState.data
-                currentUiState.copy(data = olData.copy(showConfirmationDialog = true))
-            } else currentUiState
-        }
-    }
-
-    fun reserveCourt() {}// mandar a fire
-
-    fun dismissDialog() {
-        _state.update { currentUiState ->
-            if (currentUiState is BookingUiState.Success) {
-                val olData = currentUiState.data
-                currentUiState.copy(data = olData.copy(showConfirmationDialog = false))
-            } else currentUiState
-        }
-    }//false al dialog
-
-    fun confirmDialog() {} // meter el reserveCourt
-
-    //Activa la hora elegida
-    private fun onSelectedHourToReserve(
-        listFreeHour: List<FreeHoursOfCourt>,
-        selectedHour: String?
-    ): List<FreeHoursOfCourt> {
-        val options = mutableListOf<FreeHoursOfCourt>()
-        options.addAll(
-            listFreeHour.map {
-                FreeHoursOfCourt(
-                    code = it.code,
-                    hour = it.hour,
-                    isFree = it.isFree,
-                    isSelected = selectedHour == it.hour && !it.isSelected
-                )
-            }
-        )
-        return options
-    }
-}*/
+}
